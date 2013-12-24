d61172a610c242f8c4ca642c03e9bcec1d7f9770
acuberof <acuberof@gmail.com>
2013-12-24 11:20:35 +0100

    Delete rxeflash.sh

--- Practica1/Practica1a/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload practica1a.rxe...
-wineconsole /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=practica1a.rxe
-wineconsole /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=practica1a.rxe
-echo NeXTTool is terminated.
