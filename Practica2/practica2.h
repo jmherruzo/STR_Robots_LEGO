476d6497ecee6a58cb677917e85000beb9b854ca
jmherruzo <josemaherruzo@hotmail.com>
2013-12-30 18:47:07 +0100

    Añadido .h

---
+++ Practica2/practica2.h
@@ -0,0 +1,34 @@
+
+//Variable para transformar de radianes a grados
+#define RADS 57.2957795
+//Longitud de la circunferencia de la rueda
+#define RUEDA 17.5
+//Limite superior de velocidad
+#define UPPER_LIMIT 100
+//Limites de la "zona de confort"
+#define LIMITE_CERCA 15
+#define LIMITE_LEJOS 18
+#define LIMITE_MAX 40
+#define DIS_MEDIA 17
+//Velocidades iniciales de los motores
+#define SPEEDB 49
+#define SPEEDC 50
+//Maxima correccion que se aplicara a los motores
+#define MAX_CORRECCION 3
+//Datos para selecionar una tarea u otra
+#define DIR_DERECHA 0
+#define DIR_IZQUIERDA 1
+#define DIR_REC_DER 2
+#define DIR_REC_IZ 3
+//Puertos usados
+#define SONAR_PORT NXT_PORT_S4
+#define PULSADOR1_PORT NXT_PORT_S1
+
+int getDistancia();
+int getMediumRevs();
+void correccionDerecha();
+void correccionIzquierda();
+void setVelocidad(int motorB, int motorC);
+void resetMotorCounters();
+int calcularGradosCorreccion(int difDistancia, int difRevs);
+void resetOldValues();
